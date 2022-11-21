0ea1fa057a4b9ca8ba4b8951d4ea5a0780eb58eb
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2022-11-21 01:10:58 +0100

    Added Entity Framework Core

--- ShoppingList-API/ShoppingListApp/Program.cs
+++ ShoppingList-API/ShoppingListApp/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using ShoppingListApp.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +17,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options => builder.Configuration.Bind("JwtSettings", options))
     .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
         options => builder.Configuration.Bind("CookieSettings", options));
+
+builder.Services.AddDbContext<AppDbContext>(b =>
+    b.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("PostgreSQL")));
 
 var app = builder.Build();
 
